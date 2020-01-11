ab1a4e114beaeeca7da4718fe9a861ea5e5c4dcd
IronTony <zimzum4@gmail.com>
2020-01-11 10:46:02 +0100

    Created folders structure and modified babel config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ios.js', '.android.js', '.ios.jsx', '.android.jsx', '.js', '.jsx', '.json'],
+        root: ['.'],
+        alias: {
+          '@api': './src/api',
+          '@components': './src/components',
+          '@i18n': './src/i18n',
+          '@redux': './src/redux',
+          '@routes': './src/routes',
+          '@scenes': './src/scenes',
+          '@services': './src/services',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
