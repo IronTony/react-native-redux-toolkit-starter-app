052539423e829e26e91db95f5d267b3e2508157a
Mattia Panzeri <mattia.panzeri93@gmail.com>
2020-01-17 23:01:29 +0100

    feat: Add more examples to rootSaga

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -4,4 +4,6 @@ import userSagas from '@redux/user/sagas';
 
 export default function* rootSaga() {
   yield fork(userSagas);
+  // yield fork(someModuleSagas);
+  // yield fork(someOtherModuleSagas);
 }
