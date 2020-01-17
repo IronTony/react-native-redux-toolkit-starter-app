39b2f3f96e6821260b00ec6769a525879925724e
Mattia Panzeri <mattia.panzeri93@gmail.com>
2020-01-17 20:20:44 +0100

    feat: Add 'createLatestSagas' and 'createEverySagas' methods to 'redux' utils

--- src/utils/redux.js
+++ src/utils/redux.js
@@ -1,4 +1,5 @@
 import produce from 'immer';
+import { all, spawn, takeEvery, takeLatest } from 'redux-saga/effects';
 
 // createAction utility func is ported from '@reduxjs/toolkit'
 //  see original source here: https://github.com/reduxjs/redux-toolkit/blob/master/src/createAction.ts#L270
@@ -57,5 +58,80 @@ export function createReducer(initialState, actionsMap) {
       const caseReducer = actionsMap[action.type];
       return caseReducer ? caseReducer(draft, action) : undefined;
     });
+  };
+}
+
+// createLatestSagas
+//  Returned rootSaga is inspired by code at https://redux-saga.js.org/docs/advanced/RootSaga.html
+
+/**
+ * A utility function that allows defining a saga as a mapping from action
+ * type to takeLatest sagas that handle these action types.
+ *
+ * @param sagasMap A callback that receives a *builder* object to define the sagas.
+ * @param moduleName An optional parameter to specify the module the generated sagas belong to.
+ *
+ * @public
+ */
+export function createLatestSagas(sagasMap, moduleName = '') {
+  // Extract action patterns from supplied map
+  const actionPatterns = Object.keys(sagasMap);
+
+  // Return moduleSaga
+  return function* moduleSaga() {
+    yield all(
+      actionPatterns.map(actionPattern =>
+        spawn(function*() {
+          while (true) {
+            try {
+              yield takeLatest(actionPattern, sagasMap[actionPattern]);
+              break;
+            } catch (e) {
+              console.log(
+                moduleName,
+                `Error executing saga for action ${actionPattern}:`,
+                e,
+              );
+            }
+          }
+        }),
+      ),
+    );
+  };
+}
+
+/**
+ * A utility function that allows defining a saga as a mapping from action
+ * type to takeEvery sagas that handle these action types.
+ *
+ * @param sagasMap A callback that receives a *builder* object to define the sagas.
+ * @param moduleName An optional parameter to specify the module the generated sagas belong to.
+ *
+ * @public
+ */
+export function createEverySagas(sagasMap, moduleName = '') {
+  // Extract action patterns from supplied map
+  const actionPatterns = Object.keys(sagasMap);
+
+  // Return moduleSaga
+  return function* moduleSaga() {
+    yield all(
+      actionPatterns.map(actionPattern =>
+        spawn(function*() {
+          while (true) {
+            try {
+              yield takeEvery(actionPattern, sagasMap[actionPattern]);
+              break;
+            } catch (e) {
+              console.log(
+                moduleName,
+                `Error executing saga for action ${actionPattern}:`,
+                e,
+              );
+            }
+          }
+        }),
+      ),
+    );
   };
 }
