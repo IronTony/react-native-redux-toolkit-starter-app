20d320545b356b6a8bbb73d35c1810678d754bac
IronTony <zimzum4@gmail.com>
2020-01-19 09:48:29 +0100

    Added RTL transition for both systems

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,27 @@
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from 'react-navigation-stack';
 import Homepage from '@scenes/Homepage';
 import OtherPage from '@scenes/OtherPage';
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: Homepage,
+const AppNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: Homepage,
+    },
+    AnotherPage: {
+      screen: OtherPage,
+    },
   },
-  AnotherPage: {
-    screen: OtherPage,
+  {
+    defaultNavigationOptions: {
+      // this is to have a RTL transition on both systems
+      ...TransitionPresets.SlideFromRightIOS,
+    },
   },
-});
+);
 
 const AppContainer = createAppContainer(AppNavigator);
 
