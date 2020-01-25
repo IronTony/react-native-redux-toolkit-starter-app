6f3fd6949a1fdafa7fc886b57034b096263566ef
Mattia Panzeri <mattia.panzeri93@gmail.com>
2020-01-25 16:33:16 +0100

    feat: Setup app-wide 'Suspense'

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { enableScreens } from 'react-native-screens';
-import { store, persistor } from '@redux/store';
 import { getTheme, StyleProvider } from 'native-base';
-import AppContainer from '@routes';
+import Splashscreen from '@components/Splashscreen';
 import '@i18n';
+import { store, persistor } from '@redux/store';
+import AppContainer from '@routes';
 
 enableScreens();
 
 const App = () => (
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <StyleProvider style={getTheme()}>
-        <AppContainer />
-      </StyleProvider>
-    </PersistGate>
-  </Provider>
+  <Suspense fallback={<Splashscreen />}>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <StyleProvider style={getTheme()}>
+          <AppContainer />
+        </StyleProvider>
+      </PersistGate>
+    </Provider>
+  </Suspense>
 );
 
 export default App;
